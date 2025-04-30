faa7e849419d4407a74c76fc59bbdef4b8892f51
majvax <majvax.contact@gmail.com>
2025-04-30 21:14:31 +0200

    Removes unnecessary base path configuration
    Removes the `basePath` and `assetPrefix` configurations from `next.config.ts` as they are no longer required with the use of a custom domain.

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'export',
     // add the name of the repository so that the static files are served from the correct path
-    basePath: '/portfolio',
-    assetPrefix: '/portfolio/',
+    // shouldn't be necessary now that I'm using a custom domain
+    // basePath: '/portfolio',
+    // assetPrefix: '/portfolio/',
     images: {
         unoptimized: true,
     },
